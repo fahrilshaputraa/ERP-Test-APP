de68c438e54f34a090f898e8b5922b6b2de8c571
fahrilshaputraa <fahrilshaputra@gmail.com>
2024-10-03 07:21:40 +0000

    first commit

---
+++ test_app/test_app/doctype/article/test_article.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Fahril Shaputra and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestArticle(FrappeTestCase):
+	pass
